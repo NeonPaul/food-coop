adf5ebfb9e6268dd508b75a7508e4d9a5b870739
Paul Kiddle <neonpaul@gmail.com>
2017-02-03 13:49:13 +0000

    Turn off autocomplete for search box

--- web/request.js
+++ web/request.js
@@ -12,11 +12,11 @@ function findPos(obj) {// http://www.quirksmode.org/js/findpos.html
 }
 
 function addEvent(el,trigger,func){
-	if (el.addEventListener){  
+	if (el.addEventListener){
 		el.addEventListener(trigger,func,false);
-	} else if (el.attachEvent){  
-		el.attachEvent('on'+trigger, func);  
-	}  
+	} else if (el.attachEvent){
+		el.attachEvent('on'+trigger, func);
+	}
 }
 
 Element.prototype.isDescendantOf = function(ancestor){
@@ -30,7 +30,7 @@ Element.prototype.isDescendantOf = function(ancestor){
 
 document.itemSearch={
 	XHR: new XMLHttpRequest(),
-	keydown:function(e){	
+	keydown:function(e){
 		if(e.keyCode==27 || e.keyCode==9){
 			document.itemSearch.resultsArea.hide();
 			return;
@@ -80,17 +80,18 @@ document.itemSearch={
 			itemElement.setAttribute("class","result");
 			itemElement.link.setAttribute("href","javascript:;");
 			itemElement.link.innerHTML=item.item_id+" - "+item.item_name+" "+item.unit_weight+item.unit_weight_unit+" - £"+item.unit_price.toFixed(2);
-			
+
 			addEvent(itemElement.link,"click",function(){return function(e){document.itemSearch.selectItem(item.item_id,itemElement.link.innerHTML);e.preventDefault;}}());
 			this.resultsElement.appendChild(itemElement);
 		},
 		show: function(){
 			document.itemSearch.resultsArea.resultsElement.style.display="block";	
-		},	
+		},
 		hide:function(){
 			document.itemSearch.resultsArea.resultsElement.style.display="none";	
 		},
-		build: function(){	
+		build: function(){
+                  document.new_item.q.setAttribute('autocomplete', 'off')
 			this.resultsElement=document.createElement("ul");
 			var qpos=findPos(document.new_item.q);
 			var top=qpos[1]+document.new_item.q.offsetHeight;
@@ -99,7 +100,7 @@ document.itemSearch={
 			this.resultsElement.setAttribute("id","item_search_results");	
 			this.hide();
 			document.getElementById("item_search").appendChild(this.resultsElement);
-			
+
 			this.moreLink=document.createElement("li");
 			this.moreLink.setAttribute("style","text-align:center;");
 			this.moreLink.link=document.createElement("a");
@@ -108,13 +109,12 @@ document.itemSearch={
 			this.moreLink.link.setAttribute("href","#");
 			this.moreLink.link.setAttribute("style","font-weight:bold;");
 			this.moreLink.link.innerHTML="Search the full Infinity catalogue.";
-			
+
 			this.moreLink.add=function(t){return function(searchTerm){
-				
-t.moreLink.link.setAttribute("href","/request/search_catalogue?q="+escape(searchTerm));
+
+t.moreLink.link.setAttribute("href","request/search_catalogue?q="+escape(searchTerm));
 				t.resultsElement.appendChild(t.moreLink);
 			}}(this);
-			
 		}
 	}
 }
@@ -123,24 +123,24 @@ function deleteItem(itemForm){
 	itemForm.submit();
 }
 
-function init(){	
+function init(){
 	var forms=document.getElementsByTagName("form");
 	for(var i=0;i<forms.length;i++){
 		if(forms[i].className.indexOf("request_contents_row")==-1)
 			continue;
 		addEvent(forms[i].deleteButton,"click",deleteItem.bind(null,forms[i]));
 	}
-	
+
 	document.itemSearch.resultsArea.build();
 	addEvent(document.new_item.q,"focus",function(){return document.itemSearch.focus}());
 	addEvent(document.new_item.q,"keydown",function(){return document.itemSearch.keydown}());
 	addEvent(document.new_item.q,"keyup",function(){return document.itemSearch.initiateRequest}());
 
-	
+
 	if(document.new_item.submit_b){
-		document.new_item.action="/request/add";
+		document.new_item.action="request/add";
 		document.new_item.submit_b.value="Add";
-		document.getElementById("quantity").style.display="inline";		
+		document.getElementById("quantity").style.display="inline";
 	}
 }
 
